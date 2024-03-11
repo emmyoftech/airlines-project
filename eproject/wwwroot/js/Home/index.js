b1798cc91b5a64d9e6d111e863e247c8fcf624f9
hp <emmanuelbowofoluwa@gmail.com>
2024-03-11 02:34:00 +0100

    lines 2

--- eproject/wwwroot/js/Home/index.js
+++ eproject/wwwroot/js/Home/index.js
@@ -138,4 +138,23 @@ $(document).ready(function(){
             }
         }
     });
+
+    $(".planeHld").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
 });

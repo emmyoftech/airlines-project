a850c7d19cd9a65834549bc23184bfd7b88e3c86
EMMANUEL <emmanuelcod90@gmail.com>
2024-03-10 23:55:26 -0800

    owl3 stage

--- eproject/wwwroot/js/Home/index.js
+++ eproject/wwwroot/js/Home/index.js
@@ -119,19 +119,23 @@ function setupContact(){
     }
 }
 
-// $(".portslist").owlCarousel({
-//     loop: true,
-//     margin: 10,
-//     nav: false,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 3
-//         },
-//         1000: {
-//             items: 5
-//         }
-//     }
-//    })
+$(document).ready(function(){
+    $('.portslist').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
